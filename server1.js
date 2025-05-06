802f048157acc4a986cfd3edffb74a92e75912d3
shaikhshafique98 <shaikhshafique98@gmail.com>
2025-05-06 12:53:49 +0530

    Update server1.js

--- server1.js
+++ server1.js
@@ -4,8 +4,7 @@ const express = require('express');
 const mysql   = require('mysql');
 const cors    = require('cors');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-const path = require('path');
+
 
 const app = express();
 const PORT = 3000;
